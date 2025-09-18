47d7a03b5e4a5a6b88a264ea3562d5390a4652e2
brant <brant@email.com>
2025-09-18 19:17:47 -0300

    dia 17/09 concluído

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,8 +1,12 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sobre_nos/', views.sobre_nos, name='sobre_nos'),
+    path('contato/', views.contato, name='contato'),
+    path('sucesso/', views.sucesso, name='sucesso'),
     path('receitas/<int:id>/', views.receita_detail, name='receita_detail'),
     path('pesquisar/', views.pesquisar_receitas, name='pesquisar_receitas'),
 ]
